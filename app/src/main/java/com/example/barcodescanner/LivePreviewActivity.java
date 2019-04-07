629d3c05c0b1f00fafa73104558e0229f5d39e70
zeroc <mmeza.developer@gmail.com>
2019-04-07 02:40:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/barcodescanner/LivePreviewActivity.java
@@ -0,0 +1,239 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.example.barcodescanner;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.example.barcodescanner.barcodescanning.BarcodeScanningProcessor;
+import com.example.barcodescanner.common.CameraSource;
+import com.example.barcodescanner.common.CameraSourcePreview;
+import com.example.barcodescanner.common.GraphicOverlay;
+import com.example.barcodescanner.common.OverlayView;
+import com.google.android.gms.common.annotation.KeepName;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Demo app showing the various features of ML Kit for Firebase. This class is used to
+ * set up continuous frame processing on frames from a camera source. */
+@KeepName
+public final class LivePreviewActivity extends AppCompatActivity
+    implements OnRequestPermissionsResultCallback,
+        CompoundButton.OnCheckedChangeListener, BarcodeScanningProcessor.BarcodeListener {
+  private static final String BARCODE_DETECTION = "Barcode Detection";
+  public static final String BARCODE = "BARCODE";
+  private static final String TAG = "LivePreviewActivity";
+  private static final int PERMISSION_REQUESTS = 1;
+
+  public static int SUCCESS_BARCODE_DETECTON=1;
+  public static int FAILED_BARCODE_DETECTON=0;
+
+  private CameraSource cameraSource = null;
+  private CameraSourcePreview preview;
+  private GraphicOverlay graphicOverlay;
+  private String selectedModel = BARCODE_DETECTION;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate");
+
+    setContentView(R.layout.activity_live_preview);
+
+    preview = (CameraSourcePreview) findViewById(R.id.firePreview);
+    if (preview == null) {
+      Log.d(TAG, "Preview is null");
+    }
+    graphicOverlay = (GraphicOverlay) findViewById(R.id.fireFaceOverlay);
+    if (graphicOverlay == null) {
+      Log.d(TAG, "graphicOverlay is null");
+    }
+
+    OverlayView overlayView=(OverlayView) findViewById(R.id.overlayView);
+
+    // Hide the toggle button if there is only 1 camera
+
+    if (allPermissionsGranted()) {
+      createCameraSource(selectedModel);
+    } else {
+      getRuntimePermissions();
+    }
+  }
+
+  @Override
+  public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+    Log.d(TAG, "Set facing");
+    if (cameraSource != null) {
+      if (isChecked) {
+        cameraSource.setFacing(CameraSource.CAMERA_FACING_FRONT);
+      } else {
+        cameraSource.setFacing(CameraSource.CAMERA_FACING_BACK);
+      }
+    }
+    preview.stop();
+    startCameraSource();
+  }
+
+  private void createCameraSource(String model) {
+    // If there's no existing cameraSource, create one.
+    if (cameraSource == null) {
+      cameraSource = new CameraSource(this, graphicOverlay);
+    }
+
+      switch (model) {
+
+        case BARCODE_DETECTION:
+          Log.i(TAG, "Using Barcode Detector Processor");
+          cameraSource.setMachineLearningFrameProcessor(new BarcodeScanningProcessor(this));
+          break;
+        default:
+          Log.e(TAG, "Unknown model: " + model);
+      }
+  }
+
+  /**
+   * Starts or restarts the camera source, if it exists. If the camera source doesn't exist yet
+   * (e.g., because onResume was called before the camera source was created), this will be called
+   * again when the camera source is created.
+   */
+  private void startCameraSource() {
+    if (cameraSource != null) {
+      try {
+        if (preview == null) {
+          Log.d(TAG, "resume: Preview is null");
+        }
+        if (graphicOverlay == null) {
+          Log.d(TAG, "resume: graphOverlay is null");
+        }
+        preview.start(cameraSource, graphicOverlay);
+      } catch (IOException e) {
+        Log.e(TAG, "Unable to start camera source.", e);
+        cameraSource.release();
+        cameraSource = null;
+      }
+    }
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume");
+    startCameraSource();
+  }
+
+  /** Stops the camera. */
+  @Override
+  protected void onPause() {
+    super.onPause();
+    preview.stop();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    if (cameraSource != null) {
+      cameraSource.release();
+    }
+  }
+
+  private String[] getRequiredPermissions() {
+    try {
+      PackageInfo info =
+          this.getPackageManager()
+              .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+      String[] ps = info.requestedPermissions;
+      if (ps != null && ps.length > 0) {
+        return ps;
+      } else {
+        return new String[0];
+      }
+    } catch (Exception e) {
+      return new String[0];
+    }
+  }
+
+  private boolean allPermissionsGranted() {
+    for (String permission : getRequiredPermissions()) {
+      if (!isPermissionGranted(this, permission)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private void getRuntimePermissions() {
+    List<String> allNeededPermissions = new ArrayList<>();
+    for (String permission : getRequiredPermissions()) {
+      if (!isPermissionGranted(this, permission)) {
+        allNeededPermissions.add(permission);
+      }
+    }
+
+    if (!allNeededPermissions.isEmpty()) {
+      ActivityCompat.requestPermissions(
+          this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(
+          int requestCode, String[] permissions, int[] grantResults) {
+    Log.i(TAG, "Permission granted!");
+    if (allPermissionsGranted()) {
+      createCameraSource(selectedModel);
+    }
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+  }
+
+  private static boolean isPermissionGranted(Context context, String permission) {
+    if (ContextCompat.checkSelfPermission(context, permission)
+        == PackageManager.PERMISSION_GRANTED) {
+      Log.i(TAG, "Permission granted: " + permission);
+      return true;
+    }
+    Log.i(TAG, "Permission NOT granted: " + permission);
+    return false;
+  }
+
+  @Override
+  public void onSuccess(FirebaseVisionBarcode barcode) {
+    Intent intent=new Intent();
+    intent.putExtra(BARCODE,barcode.getRawValue());
+    setResult(SUCCESS_BARCODE_DETECTON,intent);
+    finish();
+  }
+
+  @Override
+  public void onFailure() {
+    setResult(FAILED_BARCODE_DETECTON);
+    finish();
+  }
+}
---
+++ app/src/main/java/com/example/barcodescanner/barcodescanning/BarcodeScanningProcessor.java
@@ -0,0 +1,160 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.example.barcodescanner.barcodescanning;
+
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.example.barcodescanner.VisionProcessorBase;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.example.barcodescanner.common.CameraImageGraphic;
+import com.example.barcodescanner.common.FrameMetadata;
+import com.example.barcodescanner.common.GraphicOverlay;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Barcode Detector Demo.
+ */
+public class BarcodeScanningProcessor extends VisionProcessorBase<List<FirebaseVisionBarcode>> {
+
+    private static final String TAG = "BarcodeScanProc";
+
+    private final FirebaseVisionBarcodeDetector detector;
+
+    private BarcodeListener mBarcodeListener;
+
+    public BarcodeScanningProcessor(BarcodeListener barcodeListener) {
+        // Note that if you know which format of barcode your app is dealing with, detection will be
+        // faster to specify the supported barcode formats one by one, e.g.
+        // new FirebaseVisionBarcodeDetectorOptions.Builder()
+        //     .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
+        //     .build();
+        detector = FirebaseVision.getInstance().getVisionBarcodeDetector();
+        mBarcodeListener=barcodeListener;
+    }
+
+    @Override
+    public void stop() {
+        try {
+            detector.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Exception thrown while trying to close Barcode Detector: " + e);
+        }
+    }
+
+    @Override
+    protected Task<List<FirebaseVisionBarcode>> detectInImage(FirebaseVisionImage image) {
+        return detector.detectInImage(image);
+    }
+
+    @Override
+    protected void onSuccess(
+            @Nullable Bitmap originalCameraImage,
+            @NonNull List<FirebaseVisionBarcode> barcodes,
+            @NonNull FrameMetadata frameMetadata,
+            @NonNull GraphicOverlay graphicOverlay) {
+
+        graphicOverlay.clear();
+        if (originalCameraImage != null) {
+            CameraImageGraphic imageGraphic = new CameraImageGraphic(graphicOverlay, originalCameraImage);
+            graphicOverlay.add(imageGraphic);
+        }
+        /*
+        for (int i = 0; i < barcodes.size(); ++i) {
+            FirebaseVisionBarcode barcode = barcodes.get(i);
+            BarcodeGraphic barcodeGraphic = new BarcodeGraphic(graphicOverlay, barcode);
+            graphicOverlay.add(barcodeGraphic);
+        }
+        */
+        graphicOverlay.postInvalidate();
+        List<FirebaseVisionBarcode> lBarcodes=selectFocus(barcodes,frameMetadata);
+        if(lBarcodes.size()>0){
+            mBarcodeListener.onSuccess(barcodes.get(0));
+
+        }
+
+    }
+
+    public List<FirebaseVisionBarcode> selectFocus(List<FirebaseVisionBarcode> barcodes,FrameMetadata metadata) {
+
+        List<FirebaseVisionBarcode> finalListBarcode=new ArrayList<FirebaseVisionBarcode>();
+        double nearestDistance = distanceSelectedAreaToCenter(metadata);
+
+        Log.w("Distance: ","nearestDistance: "+nearestDistance);
+
+        for (int i = 0; i < barcodes.size(); i++) {
+            FirebaseVisionBarcode barcode = barcodes.get( i);
+            float dx = Math.abs((metadata.getWidth() / 2) - barcode.getBoundingBox().centerX());
+            float dy = Math.abs((metadata.getHeight() / 2) - barcode.getBoundingBox().centerY());
+
+            double distanceFromCenter =  Math.sqrt((dx * dx) + (dy * dy));
+
+            Log.w("Distance: ","barcode.getBoundingBox().centerX(): "+barcode.getBoundingBox().centerX());
+            Log.w("Distance: ","barcode.getBoundingBox().centerY(): "+barcode.getBoundingBox().centerY());
+            Log.w("Distance: ","distanceFromCenter: "+distanceFromCenter);
+
+            if (distanceFromCenter <= nearestDistance) {
+                finalListBarcode.add(barcode);
+            }
+        }
+        return finalListBarcode;
+    }
+
+    public double distanceSelectedAreaToCenter(FrameMetadata frameMetadata){
+        int width=0;
+        int height=0;
+
+
+
+        int widthCenter=frameMetadata.getWidth()/2;
+        int heightCenter=frameMetadata.getHeight()/2;
+        if(frameMetadata.getRotation()==90){
+            width=(int)(frameMetadata.getWidth()*0.3);
+            height=(int)(frameMetadata.getHeight()/10);
+        }else{
+            width=(int)(frameMetadata.getWidth()/10);
+            height=(int)(frameMetadata.getHeight()*0.3);
+        }
+
+        int dx=Math.abs((widthCenter-width));
+        int dy=Math.abs((heightCenter-height));
+
+        Log.w("Distance: ","distanceSelectedAreaToCenter: ");
+        Log.w("Distance: ","widthCenter: "+widthCenter);
+        Log.w("Distance: ","heightCenter: "+heightCenter);
+        Log.w("Distance: ","width: "+width);
+        Log.w("Distance: ","heightCenter: "+height);
+
+        return Math.sqrt((dx*dx)+(dy*dy));
+    }
+
+    @Override
+    protected void onFailure(@NonNull Exception e) {
+        Log.e(TAG, "Barcode detection failed " + e);
+    }
+
+    public static interface BarcodeListener{
+        public void onSuccess(FirebaseVisionBarcode barcode);
+        public void onFailure();
+    }
+}
